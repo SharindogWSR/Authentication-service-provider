dceefd5b508c0c50933d6a6f7f3dc867c5c0c810
Egor Repiev <egor.repiev@gmail.com>
2022-01-10 18:26:31 +0000

    Обновление кода.

--- html/controllers/router.php
+++ html/controllers/router.php
@@ -4,14 +4,14 @@
     Все пути попадают в $_GET['path_controller'].
   */
 
-  namespace nttek/controllers/router;
+  namespace nttek\controllers\router;
 
   class Pathfinder {
     private $directory = __DIR__ . '/../routers';
-    private $finder = $_GET['path_controller'];
+    private $finder = !empty($_GET['path_controller']) ? $_GET['path_controller'] : '';
 
     public function __construct() {
-      if (!empty($_GET['path_controller'])) {
+      if (!empty($this -> finder)) {
         if (file_exists($this -> get_path($this -> finder))) {
           require $this -> get_path($this -> finder);
         } else {
@@ -29,8 +29,8 @@
     private function show_code(int $code = 404) {
       http_response_code($code);
       print(json_encode([
-        'answer' => 'Смотри в HTTP Response Code.';
-        'code' => $code;
+        'answer' => 'Смотри в HTTP Response Code.',
+        'code' => $code,
       ]));
     }
   }
--- html/index.php
+++ html/index.php
@@ -1,1 +1,8 @@
 <?php
+  namespace nttek;
+
+  header("Content-Type: application/json");
+
+  require __DIR__ . '/controllers/router.php';
+
+  new \nttek\controllers\router\Pathfinder();
