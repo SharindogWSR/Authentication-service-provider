14e73ae4f572750db17d2537a7cf2a7fdd9c77f5
SharindogWSR <99674481+SharindogWSR@users.noreply.github.com>
2022-02-16 22:01:51 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-docker build -t nttek/auth . # tag, лучше везде указать один, чтобы не запутаться, например firstver
+docker build -t nttek/auth . # tag, лучше везде указать один (и внизу), чтобы не запутаться, например firstver
 docker run -dp 8080:80 --name nttek_auth nttek/auth # 8080 - внешний, внутри слушает 80
